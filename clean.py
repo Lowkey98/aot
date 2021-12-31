77d100e7af7aea755aaa1499b03edf369740a688
lowkey98 <ayoubafdel@gmail.com>
2021-12-31 23:07:48 +0000

    wordcloud season 1

--- clean.py
+++ clean.py
@@ -1,9 +1,11 @@
 import os
 
-directory = "/Users/ayafdel/Desktop/subs/aots01"
-for filename in os.listdir(directory):
-    with open(f'{directory}/{filename}') as input:
-        with open(f'{filename[:-4]}',"a") as output:
+PWD = os.getenv('PWD')
+input_dir = PWD +"/aots2"
+output_dir = PWD + "/aot_cleaned/s2"
+for filename in os.listdir(input_dir):
+    with open(f'{input_dir}/{filename}') as input:
+        with open(f'{output_dir}/{filename[:-4]}',"a") as output:
             sep = ",,"
             for single_line in input:
                 if sep in single_line:
--- clean_files.c
+++
@@ -1,4 +0,0 @@
-with open("aots01/aot01.ass","r") as f:
-    sep = ",,"
-    for single_line in f:
-        if sep in single_line:
